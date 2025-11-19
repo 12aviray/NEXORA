769f2f95e4d19016f4edc7a3f5e409cdfcf5d90c
12aviray <avishekrayab1972@gmail.com>
2025-11-20 02:01:35 +0600

    Implement global variables and UI helper functions

--- shared.c
+++ shared.c
@@ -1,12 +1,20 @@
 #include "shared.h"
 
-// Define Global Variables
 TodoItem todo_list[MAX_TASKS];
 int todo_count = 0;
 pthread_mutex_t todo_list_mutex = PTHREAD_MUTEX_INITIALIZER;
+char current_user_name[100] = "";
 
-// Helper Function
 void clear_stdin_buffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void clear_screen() {
+    // Detects OS to run correct clear command
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
